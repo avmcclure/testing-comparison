ce8e691456c93abe994426f182e14aeeab174e1b
Tim Andersen <timander@gmail.com>
2021-10-08 18:26:59 -0500

    fixed broken test

--- java/src/test/java/com/services/PlaceholderServiceTest.java
+++ java/src/test/java/com/services/PlaceholderServiceTest.java
@@ -3,26 +3,65 @@ package com.services;
 import com.models.PhotoResponse;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.*;
 
 public class PlaceholderServiceTest {
     private final int albumId = 2;
     private final String url = "https://jsonplaceholder.typicode.com/photos?albumId=%s";
     private PlaceholderService service;
-    private RestTemplate mockRest;
+    private RestTemplate restTemplate;
 
     @Before
     public void setup() {
-        mockRest = mock(RestTemplate.class);
-        service = new PlaceholderService(mockRest);
+        restTemplate = mock(RestTemplate.class);
+        service = new PlaceholderService(restTemplate);
     }
 
     @Test
     public void getPhotosByAlbum_ShouldMakeCallToRestTemplate() {
-        service.getPhotosByAlbum(albumId);
-        verify(mockRest).getForEntity(String.format(url, albumId), PhotoResponse[].class);
+        PhotoResponse photoResponse = new PhotoResponse();
+        photoResponse.setAlbumId(this.albumId);
+        photoResponse.setTitle("Maybe Someday");
+        photoResponse.setId(37);
+        photoResponse.setThumbnailUrl("pretend this is your thumbnail url");
+        photoResponse.setUrl("pretend this is your album url");
+
+        ResponseEntity<PhotoResponse[]> fakeResponse = createFakeResponseWith(photoResponse);
+        when(restTemplate.getForEntity(String.format(url, albumId), PhotoResponse[].class)).thenReturn(fakeResponse);
+        PhotoResponse[] photosByAlbum = service.getPhotosByAlbum(albumId);
+
+        assertSame(photosByAlbum[0], photoResponse);
     }
+
+    @Test
+    public void getPhotosByAlbum_ShouldReturnResponseFromApiCall() {
+        var response = new PhotoResponse();
+        PhotoResponse[] collection = new PhotoResponse[] {response};
+        var entity = new ResponseEntity<>(collection, HttpStatus.OK);
+
+        when(restTemplate.getForEntity(String.format(url, albumId), PhotoResponse[].class)).thenReturn(entity);
+        var actual = service.getPhotosByAlbum(albumId);
+
+        assertArrayEquals(collection, actual);
+    }
+
+    public ResponseEntity<PhotoResponse[]> createFakeResponseWith(PhotoResponse... photoResponses) {
+        String albumUrl = String.format(url, albumId);
+        ResponseEntity<PhotoResponse[]> fakeResponse = new ResponseEntity<>(HttpStatus.OK) {
+            @Override
+            public PhotoResponse[] getBody() {
+                return photoResponses;
+            }
+        };
+        return fakeResponse;
+
+    }
+
+
 }
