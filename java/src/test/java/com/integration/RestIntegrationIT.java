debe4e8a587f0696f375928371dd01e54298a8a5
Jon Graf <jgraf@leantechniques.com>
2021-10-12 11:47:55 -0500

    added integration test to ensure endpoint returns randomly generated objects

--- java/src/test/java/com/integration/RestIntegrationIT.java
+++ java/src/test/java/com/integration/RestIntegrationIT.java
@@ -1,13 +1,20 @@
 package com.integration;
 
 import com.Application;
+import com.models.PhotoResponse;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,14 +25,30 @@ public class RestIntegrationIT {
     @LocalServerPort
     private int port;
 
+    private RestTemplate restTemplate;
+
+    @Before
+    public void setUp() {
+        restTemplate = new RestTemplate();
+    }
+
     @Test
     public void getHealth_ShouldReturnSuccessMessage() {
         var url = String.format("http://localhost:%d/healthCheck", port);
-        var restTemplate = new RestTemplate();
 
         var actual = restTemplate.getForEntity(url, String.class);
 
         assertEquals(HttpStatus.OK, actual.getStatusCode());
         assertEquals("Success", actual.getBody());
+    }
+
+    @Test
+    public void getPhotoAlbum_ShouldReturnPhotos() {
+        var albumId = 2;
+        var url = String.format("http://localhost:%d/album/%d/photos", port, albumId);
+
+        var actual = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<PhotoResponse>>() {});
+
+        assertEquals(50, actual.getBody().size());
     }
 }
